fe6604020de6a76d6af907c33e91569a62dc7e8e
Alan <alanpalazon@gmail.com>
2021-04-21 02:01:58 +0100

    Need to figure out how to use error handling in playerPlay() and then all functions nedded to play a round of Insect, Toad or SNake are complete.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -11,31 +11,41 @@ function randomNumber(min,max){
 
 function computerPlay(){
     let computerSelect = options[randomNumber(0,3)];
-    console.log(computerSelect);
+    return computerSelect;
 }
 
 
-//FUNCTION UNFINISHED
-// function playerPlay(){
-//         let askUser = prompt("Please enter either 'toad', 'snake' or 'insect'");
-//         let userAnswer = askUser.toUpperCase();
-//         if (userAnswer == options[0] || userAnswer == options[1|| userAnswer == options[2]){
-//             console.log(`You chose ${ userAnswer}`);
-//         } else askUser;
-        
-//     return askUser;
+function userInput(){ //call this function within playerPlay()
+    let askUser = prompt("Please enter either 'toad', 'snake' or 'insect'");
+    let userAnswer = askUser.charAt(0).toUpperCase() + askUser.slice(1);
+    return userAnswer; 
+}
+
+
+// //FUNCTION UNFINISHED - Need to add error handling and test 
+// function playerPlay(playerAnswer){
+//        try{ if (playerAnswer != options[0] || playerAnswer != options[1]|| playerAnswer != options[2])
+           
+//        catch{ userInput() }
+       
+//             // console.log(`You chose ${ playerAnswer}`);          
+//             // return playerAnswer;   
 // }
 
 
-// computerPlay() & playerPlay() need to be passed as params for playRound()
-function playRound(playerSelection, computerSelection){
-    if (playerSelection === computerSelection){
-        console.log("It's a draw") //add score increment 
-    } 
-    else if (playerSelection === options[0] && computerSelection === options[1]){ console.log("You win, Toad kills Insect!")}//add score increment 
-    else if (playerSelection === options[1] && computerSelection === options[2]){ console.log("You win, Insect kills Snake!")}//add score increment
-    else if (playerSelection === options[2] && computerSelection === options[0]){ console.log("You win, Snake kills Toad!")}//add score increment
 
-    else console.log(`You lose, ${ computerSelection } kills ${ playerSelection}!`);//add score increment     
+
+
+
+// function working
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == computerSelection){ console.log("It's a draw")} //add score increment 
+    else if (playerSelection == options[0] && computerSelection == options[1]){ console.log("You win, Toad kills Insect!")}//add score increment 
+    else if (playerSelection == options[1] && computerSelection == options[2]){ console.log("You win, Insect kills Snake!")}//add score increment
+    else if (playerSelection == options[2] && computerSelection == options[0]){ console.log("You win, Snake kills Toad!")}//add score increment
+
+    else console.log(`You chose ${ playerSelection}. You lose, ${ computerSelection } kills ${ playerSelection}!`);//add score increment     
 
 }
+
+// playRound(playerPlay(), computerPlay());
