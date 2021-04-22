cb0e71497fe1474b5275b9edb2fea94566ecf5db
Alan <alanpalazon@gmail.com>
2021-04-23 00:50:00 +0100

    All functions needed to play 1 round are tested and working. Next task: create game()

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -2,50 +2,48 @@
 const options = ["Toad", "Insect","Snake"];
 
 
-//min and max will be 0 and 3 - this function will be called within computerPlay() 
+
+//FUNCTIONS 
+
+//random number generator for computer turn 
 function randomNumber(min,max){
     let newRandNum = Math.floor(Math.random()* (max - min) + min);
     return newRandNum;
 }
 
 
+//computer randomly chooses 
 function computerPlay(){
     let computerSelect = options[randomNumber(0,3)];
     return computerSelect;
 }
 
 
-function userInput(){ //call this function within playerPlay()
-    let askUser = prompt("Please enter either 'toad', 'snake' or 'insect'");
-    let userAnswer = askUser.charAt(0).toUpperCase() + askUser.slice(1);
-    return userAnswer; 
+//player inputs choice
+function playerPlay(playerAnswer){
+    while(playerAnswer !== options[0] && playerAnswer !== options[1] && playerAnswer !== options[2]){
+          playerAnswer = prompt("Please enter either 'toad', 'snake' or 'insect'");
+          playerAnswer = playerAnswer.charAt(0).toUpperCase() + playerAnswer.slice(1);} 
+    if(playerAnswer === options[0] || playerAnswer === options[1] || playerAnswer === options[2]){console.log(`You choose ${ playerAnswer}.`); return playerAnswer;}  
+    if(playerAnswer == null || playerAnswer == undefined){console.log("You forfeited the round")};
 }
 
 
-// //FUNCTION UNFINISHED - Need to add error handling and test 
-// function playerPlay(playerAnswer){
-//        try{ if (playerAnswer != options[0] || playerAnswer != options[1]|| playerAnswer != options[2])
-           
-//        catch{ userInput() }
-       
-//             // console.log(`You chose ${ playerAnswer}`);          
-//             // return playerAnswer;   
+// function to play single round 
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == computerSelection){ console.log("It's a draw")} //need to add score increment 
+    else if (playerSelection == options[0] && computerSelection == options[1]){ console.log("You win, Toad kills Insect!")}//need to add score increment 
+    else if (playerSelection == options[1] && computerSelection == options[2]){ console.log("You win, Insect kills Snake!")}//need to add score increment
+    else if (playerSelection == options[2] && computerSelection == options[0]){ console.log("You win, Snake kills Toad!")}//need to add score increment
+    else console.log(`You chose ${ playerSelection}. You lose, ${ computerSelection } kills ${ playerSelection}!`);//need to add score increment     
+}
+
+
+// //function game(param){ //willl need loop until score of 5 by either player
+
 // }
 
+//FUNCTION CALLS
 
+playRound(playerPlay(), computerPlay());
 
-
-
-
-// function working
-function playRound(playerSelection, computerSelection){
-    if (playerSelection == computerSelection){ console.log("It's a draw")} //add score increment 
-    else if (playerSelection == options[0] && computerSelection == options[1]){ console.log("You win, Toad kills Insect!")}//add score increment 
-    else if (playerSelection == options[1] && computerSelection == options[2]){ console.log("You win, Insect kills Snake!")}//add score increment
-    else if (playerSelection == options[2] && computerSelection == options[0]){ console.log("You win, Snake kills Toad!")}//add score increment
-
-    else console.log(`You chose ${ playerSelection}. You lose, ${ computerSelection } kills ${ playerSelection}!`);//add score increment     
-
-}
-
-// playRound(playerPlay(), computerPlay());
