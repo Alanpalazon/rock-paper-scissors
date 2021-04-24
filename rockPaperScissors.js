e6c40c23e6da116287a06cce9c84213dc33689af
Alan <alanpalazon@gmail.com>
2021-04-24 01:03:04 +0100

    score increment added to playRound(). Need to create game loop

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -25,23 +25,44 @@ function playerPlay(playerAnswer){
           playerAnswer = prompt("Please enter either 'toad', 'snake' or 'insect'");
           playerAnswer = playerAnswer.charAt(0).toUpperCase() + playerAnswer.slice(1);} 
     if(playerAnswer === options[0] || playerAnswer === options[1] || playerAnswer === options[2]){console.log(`You choose ${ playerAnswer}.`); return playerAnswer;}  
-    if(playerAnswer == null || playerAnswer == undefined){console.log("You forfeited the round")};
+    // if(playerAnswer == null || playerAnswer == undefined){console.log("You forfeited the round")};
 }
 
 
 // function to play single round 
 function playRound(playerSelection, computerSelection){
-    if (playerSelection == computerSelection){ console.log("It's a draw")} //need to add score increment 
-    else if (playerSelection == options[0] && computerSelection == options[1]){ console.log("You win, Toad kills Insect!")}//need to add score increment 
-    else if (playerSelection == options[1] && computerSelection == options[2]){ console.log("You win, Insect kills Snake!")}//need to add score increment
-    else if (playerSelection == options[2] && computerSelection == options[0]){ console.log("You win, Snake kills Toad!")}//need to add score increment
-    else console.log(`You chose ${ playerSelection}. You lose, ${ computerSelection } kills ${ playerSelection}!`);//need to add score increment     
+    let playerScore = 0;
+    let compScore = 0;
+    if      (playerSelection == computerSelection){ console.log("It's a draw");
+             console.log(`You: ${ playerScore } Computer: ${compScore}.`);} 
+    else if (playerSelection == options[0] && computerSelection == options[1]){ console.log(`You win, ${playerSelection} kills ${computerSelection}`); 
+             playerScore++; 
+             console.log(`You: ${ playerScore } Computer: ${compScore}.`);}
+    else if (playerSelection == options[1] && computerSelection == options[2]){ console.log(`You win, ${playerSelection} kills ${computerSelection}!`);
+             playerScore++;
+             console.log(`You: ${ playerScore } Computer: ${compScore}.`);}
+    else if (playerSelection == options[2] && computerSelection == options[0]){ console.log(`You win, ${playerSelection} kills ${computerSelection}!`);
+             playerScore++;
+             console.log(`You: ${ playerScore } Computer: ${compScore}.`);}
+    else{    console.log(`You chose ${ playerSelection}. You lose, ${ computerSelection } kills ${ playerSelection}!`); 
+             compScore++;
+             console.log(`You: ${ playerScore } Computer: ${compScore}.`);}   
 }
 
 
-// //function game(param){ //willl need loop until score of 5 by either player
 
-// }
+
+// function game(param){ //willl need loop until score of 5 by either player
+//         let i = 0;
+//         while(i < 5){
+//             param;
+//         }   
+
+//         i++;
+//         if(playerScore == 5){console.log(`You win!`);}
+//         if(compScore == 5){console.log(`You lose!`);}
+
+//  }
 
 //FUNCTION CALLS
 
