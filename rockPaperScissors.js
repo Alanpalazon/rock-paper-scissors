890fc9c4b9e662997bb1c26ce19b6c665fc5d373
Alan <alanpalazon@gmail.com>
2021-04-26 22:08:21 +0100

    playerPlay() now accepting all forms of options

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,5 +1,5 @@
 // //Glob vars 
-const options = ["Toad", "Insect","Snake"];
+const options = ["toad", "insect","snake"];
 
 
 
@@ -19,52 +19,50 @@ function computerPlay(){
 }
 
 
-//player inputs choice
-function playerPlay(playerAnswer){
+//player inputs choice - Need to add null handling 
+function playerPlay(){
+    let playerAnswer = prompt("Please enter either 'toad', 'snake' or 'insect'");
+    playerAnswer = playerAnswer.toLowerCase();
     while(playerAnswer !== options[0] && playerAnswer !== options[1] && playerAnswer !== options[2]){
-          playerAnswer = prompt("Please enter either 'toad', 'snake' or 'insect'");
-          playerAnswer = playerAnswer.charAt(0).toUpperCase() + playerAnswer.slice(1);} 
+          playerAnswer = prompt("Try again");
+          playerAnswer = playerAnswer.toLowerCase();} 
     if(playerAnswer === options[0] || playerAnswer === options[1] || playerAnswer === options[2]){console.log(`You choose ${ playerAnswer}.`); return playerAnswer;}  
-    // if(playerAnswer == null || playerAnswer == undefined){console.log("You forfeited the round")};
 }
+
 
 
 // function to play single round 
 function playRound(playerSelection, computerSelection){
-    let playerScore = 0;
-    let compScore = 0;
-    if      (playerSelection == computerSelection){ console.log("It's a draw");
-             console.log(`You: ${ playerScore } Computer: ${compScore}.`);} 
-    else if (playerSelection == options[0] && computerSelection == options[1]){ console.log(`You win, ${playerSelection} kills ${computerSelection}`); 
-             playerScore++; 
-             console.log(`You: ${ playerScore } Computer: ${compScore}.`);}
-    else if (playerSelection == options[1] && computerSelection == options[2]){ console.log(`You win, ${playerSelection} kills ${computerSelection}!`);
-             playerScore++;
-             console.log(`You: ${ playerScore } Computer: ${compScore}.`);}
-    else if (playerSelection == options[2] && computerSelection == options[0]){ console.log(`You win, ${playerSelection} kills ${computerSelection}!`);
-             playerScore++;
-             console.log(`You: ${ playerScore } Computer: ${compScore}.`);}
-    else{    console.log(`You chose ${ playerSelection}. You lose, ${ computerSelection } kills ${ playerSelection}!`); 
-             compScore++;
-             console.log(`You: ${ playerScore } Computer: ${compScore}.`);}   
-}
+        let playerScore = 0;
+        let compScore = 0;
+        if      (playerSelection == computerSelection){ console.log("It's a draw");
+           console.log(`You: ${ playerScore } Computer: ${compScore}.`);} 
+        else if (playerSelection == options[0] && computerSelection == options[1]){ console.log(`You win, ${playerSelection} kills ${computerSelection}`); 
+                  playerScore++; 
+                  console.log(`You: ${ playerScore } Computer: ${compScore}.`);}
+        else if (playerSelection == options[1] && computerSelection == options[2]){ console.log(`You win, ${playerSelection} kills ${computerSelection}!`);
+                  playerScore++;
+                  console.log(`You: ${ playerScore } Computer: ${compScore}.`);}
+        else if (playerSelection == options[2] && computerSelection == options[0]){ console.log(`You win, ${playerSelection} kills ${computerSelection}!`);
+                  playerScore++;
+                  console.log(`You: ${ playerScore } Computer: ${compScore}.`);}
+        else{    console.log(`You chose ${ playerSelection}. You lose, ${ computerSelection } kills ${ playerSelection}!`); 
+                  compScore++;
+                  console.log(`You: ${ playerScore } Computer: ${compScore}.`);}  
+    }
+        
 
 
 
 
-// function game(param){ //willl need loop until score of 5 by either player
-//         let i = 0;
-//         while(i < 5){
-//             param;
-//         }   
 
-//         i++;
-//         if(playerScore == 5){console.log(`You win!`);}
-//         if(compScore == 5){console.log(`You lose!`);}
-
-//  }
+        
+       
+ 
 
 //FUNCTION CALLS
 
-playRound(playerPlay(), computerPlay());
+
+
+
 
