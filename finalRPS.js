4da3042a471a2d6a7a7293eca80345ad5e450f9b
Alan <alanpalazon@gmail.com>
2021-05-25 18:57:09 +0100

    all htm and css complete. Need to finish playAgain() and figure out how to remove events from images when a score of 5 is true

--- finalRPS.js
+++ finalRPS.js
@@ -1,7 +1,4 @@
 
- const options = document.querySelectorAll('.img-options');
- let scores = [0,0]; //find a way to make local 
-
 
 function randomNumber(min,max){
     let newRandNum = Math.floor(Math.random()*(max - min) + min);
@@ -11,60 +8,73 @@ function randomNumber(min,max){
 function computerPlay(){
     let computerSelect = options[randomNumber(0,3)];
     computerSelect = computerSelect.id
-    console.log(computerSelect);
     return computerSelect
 }
 
-function playerPlay(){
-    let playerAnswer = "hello";
-    console.log(playerAnswer);
-    return playerAnswer;    
-}
 
 function playRound(){ 
-        let playerGo = playerPlay();
+        const announcement = document.getElementById('announcement');
+        const player = document.getElementById('player');
+        const computer = document.getElementById('computer');
+        let playerGo = this.id;
         let computerGo = computerPlay();
-        computerGo;
-        playerGo;
-        if      (playerGo == computerGo){ console.log("It's a draw");
-                console.log(`You: ${scores[0]} Computer: ${scores[1]}.`); return scores;} 
-        else if (playerGo == options[0].id && computerGo == options[2].id){ console.log(`You win, insect kills snake!`); 
+        if      (playerGo == computerGo){ announcement.textContent = "It's a draw";
+                player.textContent = `PLAYER: ${ scores[0]}`; computer.textContent = `COMPUTER: ${ scores[1]}`;} 
+        else if (playerGo == options[0].id && computerGo == options[2].id){ announcement.textContent = `You win, ${playerGo} kills ${computerGo}!`; 
                 scores[0]++; 
-                console.log(`You: ${scores[0]} Computer: ${scores[1]}.`); return scores;}
-        else if (playerGo == options[1].id && computerGo == options[0].id){ console.log(`You win, toad kills insect!`);
+                player.textContent = ` PLAYER: ${ scores[0]}`; computer.textContent = ` COMPUTER: ${ scores[1]}`; 
+                announcement.textContent = `You win, ${playerGo} kills ${computerGo}!`;}
+        else if (playerGo == options[1].id && computerGo == options[0].id){ 
                 scores[0]++;
-                console.log(`You: ${scores[0]} Computer: ${scores[1]}.`); return scores;}
-        else if (playerGo == options[2].id && computerGo == options[1].id){ console.log(`You win, snake kills toad!`);
+                player.textContent = ` PLAYER: ${ scores[0]}`; computer.textContent = ` COMPUTER: ${ scores[1]}`; 
+                announcement.textContent = `You win, ${playerGo} kills ${computerGo}!`;}
+        else if (playerGo == options[2].id && computerGo == options[1].id){ 
                 scores[0]++;
-                console.log(`You: ${scores[0]} Computer: ${scores[1]}.`); return scores;}
-        else{   console.log(`You lose, ${ computerGo } kills ${ playerGo}!`); 
+                player.textContent = `PLAYER: ${ scores[0]}`; computer.textContent = `COMPUTER: ${ scores[1]}`; 
+                announcement.textContent = `You win, ${playerGo} kills ${computerGo}!`;}
+        else{   announcement.textContent =`You lose, ${ computerGo } kills ${ playerGo}!`; 
                 scores[1]++;
-                console.log(`You: ${scores[0]} Computer: ${scores[1]}.`); return scores;}        
+                player.textContent = `PLAYER: ${ scores[0]}`; computer.textContent = `COMPUTER: ${ scores[1]}`;}    
+                
+        if      (scores[0] == 5 || scores[1] == 5){
+                        if(scores[0] == 5){
+                              
+                                announcement.textContent = `You win, ${playerGo} kills ${computerGo }!  You win the game!`;
+                                // playAgain();
+                        }else if(scores[1] == 5){
+                             
+                                announcement.textContent = `You loose, ${computerGo} kills ${playerGo }! You loose the game!`;;
+                                // playAgain();
+                        }
+        }             
+        
+}
+
+//need to ass event listener to button 
+function playAgain(){
+     
+       const playAgain = document.createElement('button');
+       playAgain.textContent = "Play Again";
+       playAgain.style.cssText = "backgroundColor: rgba(19, 148, 106); borderRadius: 3px; width: 20%; height: 5%; margin: 0 auto";
+       document.getElementById('gamecontainer').appendChild(playAgain);
+       //playRound();
+       return;
 }
     
 
-
-
-// // funtion to give choice whether to play again or quit game 
-// function playAgain(){
-//     let playAgain;
-//     while(playAgain != "yes" && playAgain != "no"){
-//         playAgain = prompt("Play again? Enter 'yes' or 'no'.");     
-//         playAgain.toLowerCase();
-//     }
-//     if (playAgain == "yes"){ playGame(); return playAgain;}
-//     else if (playAgain == "no"){ console.log("Thanks for playing. Bye!"); window.alert("Game over. Press refresh to start");} 
+// function removeClick(){
+        
+//         return 1;
 // }
+
+// let remove = true;
+let scores = [0,0]; 
+const options = document.querySelectorAll('.img-options');
+options.forEach(option => option.addEventListener('click', playRound));
+// if(remove){options.forEach(option => option.removeEventListener('click', removeClick));}
 
  
 
-function playGame(){ 
-options.forEach(option => option.addEventListener('click', playRound));
-    if      (scores[0] == 5){console.log("You win the game!");return}            
-    else if (scores[1] == 5){console.log("Computer wins the game!"); return;}
-    console.log(scores);
-    return scores;
-}
 
-playGame();
+
 
